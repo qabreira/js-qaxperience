7f6d652e8439e0327935ee46fdba6dbc0413b3fe
Pablo Cabreira <cabreir@gmail.com>
2023-03-21 22:07:49 -0300

    aulas sobre variáveis e operadores matemáticos

--- js/app.js
+++ js/app.js
@@ -3,3 +3,54 @@ console.log('Olá, Javascript!')
 var userName = 'Pablo'
 
 document.getElementById('user-name').innerHTML = userName
+
+
+// variáveis //
+
+/*
+declarando variáveis
+
+var nome = 'Mestre Yoda'
+var idade = 100
+var jedi = true
+var sobrenome;
+
+identificar o tipo de valor da variável
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof jedi)
+console.log(typeof sobrenome) */
+
+// operadores matemáticos //
+/* 
+var n1 = 5
+var n2 = 4
+var n3 = '9'
+
+var soma1 = n1 + n2
+var soma2 = n1 + n3
+var soma3 = n1 + parseInt(n3) // converte string em inteiro
+
+console.log(soma1) // 9
+console.log(soma2) // '59'
+console.log(soma3) // 14
+
+var subtracao1 = n3 - n1
+var subtracao2 = parseInt(n3) - n2
+
+console.log(subtracao1) // 4
+console.log(subtracao2) // 5
+
+var multiplicacao1 = n1 * n2
+var multiplicacao2 = n3 * n1
+
+console.log(multiplicacao1) // 20
+console.log(multiplicacao2) // 45
+
+var divisao1 = n3 / n2
+var divisao2 = n3 / n3
+
+console.log(divisao1) // 2.25
+console.log(divisao2) // 1 */
+
